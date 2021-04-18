affee7e536d251966fa097813b4fa23e306770de
Mouzamuddin Mir <>
2021-04-18 10:57:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var readlinesync = require("readline-sync");
+var chalk = require("chalk")
+var touppercase = require("upper-case")
+var score=0
+var username =readlinesync.question(chalk.yellow("What is your name: "))
+console.log(chalk.red("welcome ")+chalk.blue(username)+chalk.red(" do you now mouzam"))
+var questions = 
+[
+  {question : "where do Mouzam live ",
+  answer : "hyderabad",
+  },
+  {question:"what is Mouzam's age (numeric answer only)" ,
+  answer:"21",
+  },
+  {
+  question : "how many cats does mouzam have ", 
+  answer:"two",
+  },
+  {
+  question : "how many siblings does Mouzam have??(numeric ans) ",
+  answer : "5",
+  }
+]
+function quiz(question,answer)
+{
+  var userans = readlinesync.question(question)
+  if (userans.toUpperCase() === answer.toUpperCase())
+  {
+    console.log(chalk.green("correct answer"))
+    score++
+  }
+  else
+  {
+    console.log(chalk.red("wrong answer"))
+    score--
+  }
+  console.log("current score " ,chalk.red(score))
+}
+for (i=0;i<questions.length;i++)
+{
+  var currentquestion = questions[i]
+  quiz(currentquestion.question,currentquestion.answer)
+}
+console.log(chalk.bgRed("your final score is ",score))
